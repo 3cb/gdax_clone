4ed91ce5bbd66e906f75dc5f567b8bd92eb868bd
Marc C-B <marc.cbrooks@gmail.com>
2017-10-20 11:13:13 -0400

    convert time&sales from levels to table

--- static/src/components/TradeHistory.vue
+++ static/src/components/TradeHistory.vue
@@ -14,19 +14,33 @@
         <nav class="level info is-marginless is-paddingless">
             <span class="is-uppercase has-text-weight-semibold">Trade History</span>
         </nav>
-        <nav class="level">
-            <span class="level-item">Trade Size</span>
-            <span class="level-item">Price({{ tickerDenom }})</span>
-            <span class="level-item">Time</span>
-        </nav>
-        <nav v-for="(sale, index) in sales" :key="sale.sequence" class="level">
-            <span class="level-item">{{ parseFloat(sale.size).toFixed(8) }}</span>
-            <span class="level-item"> {{ parseFloat(sale.price).toFixed(2) }}<span class="icon"><i class="fa fa-long-arrow-up" aria-hidden="true"></i></span>
-            <span class="icon"><i class="fa fa-long-arrow-down" aria-hidden="true"></i></span>
-            <span class="icon"><i class="fa fa-arrows-h" aria-hidden="true"></i></span>
-            </span>
-            <span class="level-item"> {{ sale.time | formatDate }}</span>
-        </nav>
+        <table class="is-marginless is-paddingless is-clipped">
+            <colgroup>
+                <col span="1" style="width: 10%;">
+                <col span="1" style="width: 30%;">
+                <col span="1" style="width: 30%;">
+                <col span="1" style="width: 30%;">
+            </colgroup>
+            <thead>
+                <tr>
+                    <th scope="col">{{ spacer }}</th>
+                    <th scope="col">Trade Price</th>
+                    <th scope="col">Price({{ tickerDenom }})</th>
+                    <th scope="col">Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(sale, index) in sales" :key="sale.sequence">
+                    <td>{{ spacer }}</td>
+                    <td>{{ parseFloat(sale.size).toFixed(8) }}</td>
+                    <td>{{ parseFloat(sale.price).toFixed(2) }}<span class="icon"><i class="fa fa-long-arrow-up" aria-hidden="true"></i></span>
+                        <span class="icon"><i class="fa fa-long-arrow-down" aria-hidden="true"></i></span>
+                        <span class="icon"><i class="fa fa-arrows-h" aria-hidden="true"></i></span>
+                    </td>
+                    <td>{{ sale.time | formatDate }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -34,6 +48,11 @@
 import _ from 'lodash'
 
 export default {
+  data() {
+      return {
+          spacer: ''
+      }
+  },
   filters: {
       formatDate(value) {
           return new Date(value).toLocaleTimeString()
@@ -55,5 +74,20 @@ export default {
 </script>
 
 <style>
+table {
+    table-layout: fixed;
+    width: 100%;
+}
 
+td, th {
+    padding: 5px;
+}
+
+tbody td {
+    text-align: center;
+}
+
+thead th {
+    text-align: center;
+}
 </style>
