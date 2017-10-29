408a78d897dfa8a8bef37e7d195964c91faaf92a
Marc CB <marc.cbrooks@gmail.com>
2017-10-29 14:47:13 -0400

    move top quotes from second column to third

--- static/src/components/SecondColumn.vue
+++ static/src/components/SecondColumn.vue
@@ -1,24 +1,6 @@
 <template>
 <div>
     <nav class="level info is-marginless">
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="has-text-weight-bold">{{ selectedProduct.price | decimals }} {{ tickerDenom }}</p>
-                <p class="heading">Last trade price</p>
-            </div>
-        </div>
-        <div class="level-item has-text-centered">
-            <div>
-                <p :class="deltaClass" class="has-text-weight-bold">{{ deltaSign }}{{ selectedProduct.priceDelta24h.toFixed(2) }} %</p>
-                <p class="heading">24 Hour price</p>
-            </div>
-        </div>
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="has-text-weight-bold">{{ selectedProduct.volume_24h | formatVolume }} {{ tickerSymbol }}</p>
-                <p class="heading">24 hour volume</p>
-            </div>
-        </div>
     </nav>
     <nav class="level info is-marginless is-paddingless">
         <span class="is-uppercase has-text-weight-semibold">Order Book</span>
@@ -29,45 +11,8 @@
 
 <script>
 import OrderBook from './OrderBook.vue'
-import _ from 'lodash'
-import { addCommas } from '../lib/numbers.js'
 
 export default {
-    filters: {
-        formatVolume(volume) {
-            return addCommas(Math.round(parseFloat(volume)))
-        },
-        decimals(price) {
-            if (price < 1 ) {
-                return addCommas((parseFloat(price)).toFixed(5))
-            } else {
-                return addCommas((parseFloat(price)).toFixed(2))
-            }
-        }
-    },
-    computed: {
-        selectedProduct() {
-            return this.$store.state.products[this.$store.state.selected_id-1]
-        },
-        tickerSymbol() {
-            var arr = _.split(this.selectedProduct.name, '/')
-            return arr[0]
-        },
-        tickerDenom() {
-            var arr = _.split(this.selectedProduct.name, '/')
-            return arr[1]
-        },
-        deltaSign() {
-            if (this.selectedProduct.priceDelta24h >= 0) {
-                return '+'
-            } else {
-                return ''
-            }
-        },
-        deltaClass() {
-            return this.selectedProduct.deltaClass
-        }
-    },
     components: {
         OrderBook
     }
--- static/src/components/ThirdColumn.vue
+++ static/src/components/ThirdColumn.vue
@@ -1,6 +1,31 @@
 <template>
     <div id="cont3">
-        <nav class="level info is-marginless is-paddingless"></nav>
+        <nav class="level info is-marginless is-paddingless">
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="has-text-weight-bold has-text-info">{{ selectedProduct.name }}</p>
+              <p class="heading">Current Product</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered">
+            <div>
+                <p class="has-text-weight-bold">{{ selectedProduct.price | decimals }} {{ tickerDenom }}</p>
+                <p class="heading">Last trade price</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered">
+              <div>
+                  <p :class="deltaClass" class="has-text-weight-bold">{{ deltaSign }}{{ selectedProduct.priceDelta24h.toFixed(2) }} %</p>
+                  <p class="heading">24 Hour price</p>
+              </div>
+          </div>
+          <div class="level-item has-text-centered">
+              <div>
+                  <p class="has-text-weight-bold">{{ selectedProduct.volume_24h | formatVolume }} {{ tickerSymbol }}</p>
+                  <p class="heading">24 hour volume</p>
+              </div>
+          </div>
+        </nav>
         <nav class="level info is-marginless is-paddingless">
             <span class="is-uppercase has-text-weight-semibold">Price Chart</span>
         </nav>
@@ -10,8 +35,46 @@
 
 <script>
 import Chart from "./Chart.vue";
+import _ from 'lodash'
+import { addCommas } from '../lib/numbers.js'
+
 
 export default {
+  filters: {
+        formatVolume(volume) {
+            return addCommas(Math.round(parseFloat(volume)))
+        },
+        decimals(price) {
+            if (price < 1 ) {
+                return addCommas((parseFloat(price)).toFixed(5))
+            } else {
+                return addCommas((parseFloat(price)).toFixed(2))
+            }
+        }
+    },
+    computed: {
+        selectedProduct() {
+            return this.$store.state.products[this.$store.state.selected_id-1]
+        },
+        tickerSymbol() {
+            var arr = _.split(this.selectedProduct.name, '/')
+            return arr[0]
+        },
+        tickerDenom() {
+            var arr = _.split(this.selectedProduct.name, '/')
+            return arr[1]
+        },
+        deltaSign() {
+            if (this.selectedProduct.priceDelta24h >= 0) {
+                return '+'
+            } else {
+                return ''
+            }
+        },
+        deltaClass() {
+            return this.selectedProduct.deltaClass
+        }
+    },
   components: {
     Chart
   }
