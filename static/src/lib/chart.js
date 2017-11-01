8aa3dfeb9591c7a360e9bd2dd8539ceaac15ee4e
Marc CB <marc.cbrooks@gmail.com>
2017-11-01 13:33:14 -0400

    simplify usage of momentjs

--- static/src/lib/chart.js
+++ static/src/lib/chart.js
@@ -2,19 +2,15 @@ import moment from 'moment'
 
 function getParams(interval) {
     if (interval === '1d') {
-        let arr = moment.utc().toISOString().split('T')
-        let date = arr[0]
         return {
-            start: moment(date).subtract(53, 'days').toISOString(),
-            end: moment(date).toISOString(),
+            start: moment().subtract(35, 'days').format(),
+            end: moment().format(),
             granularity: 86400
         }
     } else if (interval === '1m') {
-        let arr = moment.utc().toISOString().split('T')
-        let date = arr[0]
         return {
-            start: moment(date).subtract(100, 'minutes').toISOString(),
-            end: moment(date).toISOString(),
+            start: moment().subtract(35, 'minutes').format(),
+            end: moment().format(),
             granularity: 60
         }
     }
